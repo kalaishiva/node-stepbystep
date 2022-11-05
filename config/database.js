c70da1dd46b29350fc3a50de1ca4868b5f5a594a
designhappily <65686852+shivakalai@users.noreply.github.com>
2022-11-05 07:57:03 +0100

    MongoDb connected

--- config/database.js
+++ config/database.js
@@ -2,12 +2,14 @@ const mongoose = require('mongoose')
 
 const { MONGO_URL } = process.env
 
-mongoose.connect = (MONGO_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }).then(console.log(`DB Connection Success`))
-    .catch(error => {
-        console.log(`DB Connection Failed`)
-        console.log(error)
-        process.exit(1)
-    })
+exports.connect = () => {
+    mongoose.connect = (MONGO_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        }).then(console.log(`DB Connection Success`))
+        .catch(error => {
+            console.log(`DB Connection Failed`)
+            console.log(error)
+            process.exit(1)
+        })
+}
